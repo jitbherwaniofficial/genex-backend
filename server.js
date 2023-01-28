98f0ffdecb0110168b8c679b221b269e18a46a35
JitBherwani <jitbherwani92@gmail.com>
2023-01-28 19:19:29 +0530

    Updated

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ uri = process.env.uri
 
 
 const app = express();
-app.use(cors({
-    origin: 'https://genex.onrender.com/', // use your actual domain name (or localhost), using * is not recommended
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true
-}))
+app.use(cors())
+// app.use(cors({
+//     origin: 'https://genex.onrender.com/', // use your actual domain name (or localhost), using * is not recommended
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+//     credentials: true
+// }))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use("/user",userRoute)
