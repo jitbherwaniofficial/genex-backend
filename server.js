fab1b486acf27e8a7d2c98498c7659605c6cbd5b
JitBherwani <jitbherwani92@gmail.com>
2023-01-28 18:46:14 +0530

    Updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ uri = process.env.uri
 
 
 const app = express();
-app.use(cors({origin:"*"}))
+app.use(cors({origin:"*",credentials:true,optionsSuccessStatus:200}))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use("/user",userRoute)
