892cd3500968d2df3c7c141b1972df813cdd5939
JitBherwani <jitbherwani92@gmail.com>
2023-01-29 15:35:08 +0530

    updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 PORT = process.env.PORT || 3000
 uri = process.env.uri
 app.use(cors({
-    origin:"https://genex.onrender.com"
+    origin:"https://jitbherwaniofficial.github.io"
 }))
 
 
