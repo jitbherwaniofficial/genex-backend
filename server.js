af516bd9e3a7f726856393b340f83cf51b7b67d4
JitBherwani <jitbherwani92@gmail.com>
2023-01-30 14:09:34 +0530

    Localhost updated

--- server.js
+++ server.js
@@ -7,7 +7,21 @@ const cors = require("cors");
 
 PORT = process.env.PORT || 3000
 uri = process.env.uri
-app.use(cors({origin:"http://localhost:4200"}))
+// app.use(cors({origin:"http://localhost:4200"}))
+
+function myCors(req, res, nxt) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent');
+    if(req.method === 'OPTIONS') {
+        res.sendStatus(204);
+    }
+    else {
+        nxt();
+    }
+}
+
+app.use(myCors)
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
